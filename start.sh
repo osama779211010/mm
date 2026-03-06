2674a8f64038268aa738d6268e229d5ed99428b5
osama779211010 <www.osama779211010@gmail.com>
2026-03-06 03:15:58 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ python manage.py migrate
 # Create default admin if not exists
 python create_admin.py
 # Start Gunicorn
-gunicorn mas_project.wsgi --bind 0.0.0.0:$PORT
+gunicorn mas_project.wsgi --bind 0.0.0.0:$PORT --timeout 120
